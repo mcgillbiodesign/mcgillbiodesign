0a3bf7b158449dc1de038a8e1b9965c41ed7978c
mcgillbiodesign <biodesign@mcgilleus.ca>
2020-10-19 16:21:09 -0400

    Updates

---
+++ precache-manifest.02c5ddf5c39636817663dd8b7a323781.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "190198ceebb3fe12db857a27eefb5046",
+    "url": "/index.html"
+  },
+  {
+    "revision": "038c97fd2ad276b94711",
+    "url": "/static/css/main.5ee951d3.chunk.css"
+  },
+  {
+    "revision": "e0f42ad79dbf4c7af8cc",
+    "url": "/static/js/2.655f539d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.655f539d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "038c97fd2ad276b94711",
+    "url": "/static/js/main.3425d826.chunk.js"
+  },
+  {
+    "revision": "0f5c0937785120dcb6b4",
+    "url": "/static/js/runtime-main.37d0ebd0.js"
+  },
+  {
+    "revision": "9bc2835e0939c607e4d3b03023737e67",
+    "url": "/static/media/andrew.9bc2835e.png"
+  },
+  {
+    "revision": "f7f8e7d5fc35bf1542a4bc3f5b6dc789",
+    "url": "/static/media/anne.f7f8e7d5.png"
+  },
+  {
+    "revision": "7d42ac62c7b8cc091582ea8a53bab29e",
+    "url": "/static/media/bernadette.7d42ac62.png"
+  },
+  {
+    "revision": "69b586fc92542392b51e0dab4e4231ec",
+    "url": "/static/media/elizabeth.69b586fc.png"
+  },
+  {
+    "revision": "a1c89c75077b2e59843ccab25c0dc83f",
+    "url": "/static/media/justin.a1c89c75.png"
+  },
+  {
+    "revision": "3a5b0200000d8c2bb43722eac843680d",
+    "url": "/static/media/kimia.3a5b0200.png"
+  },
+  {
+    "revision": "c2c123dc6fe1cef56181b3193156d350",
+    "url": "/static/media/logo.c2c123dc.png"
+  },
+  {
+    "revision": "5e9058281c268d485fe1736db3677d6e",
+    "url": "/static/media/megan.5e905828.png"
+  },
+  {
+    "revision": "433529cfedf4c820396c5e2772ada50f",
+    "url": "/static/media/slide_1.433529cf.jpg"
+  },
+  {
+    "revision": "664c6c5d7d97c03f3b7ca39b064174fd",
+    "url": "/static/media/slide_2.664c6c5d.jpg"
+  },
+  {
+    "revision": "14321c2ec7a75d58d34960244b6e2369",
+    "url": "/static/media/slide_3.14321c2e.jpg"
+  },
+  {
+    "revision": "3e0d47be8b5cc4658e23d446696307d7",
+    "url": "/static/media/slide_4.3e0d47be.jpg"
+  },
+  {
+    "revision": "8ef7d543be45bcac802ab13e44d591a1",
+    "url": "/static/media/slide_5.8ef7d543.jpg"
+  },
+  {
+    "revision": "a83f544333b4de0f6ea8fbb8d9881fa5",
+    "url": "/static/media/sponsorship_package.a83f5443.pdf"
+  },
+  {
+    "revision": "f82dcdc2f12750c82254bdbe606e54a4",
+    "url": "/static/media/tirza.f82dcdc2.png"
+  },
+  {
+    "revision": "e3621aeacd523d9323f80abaa0f5ee7d",
+    "url": "/static/media/victoria.e3621aea.png"
+  },
+  {
+    "revision": "16bb2ac04a9fb2892b4ddce4a9ee5b46",
+    "url": "/static/media/zoe.16bb2ac0.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mcgillbiodesign/precache-manifest.c1a96a32e33e8c3daa9c1c6ff2ababf0.js"
+  "/precache-manifest.02c5ddf5c39636817663dd8b7a323781.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mcgillbiodesign/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
